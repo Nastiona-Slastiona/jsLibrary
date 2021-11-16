eb9a8f53204b329c7dfd8b4f68aa825f878ff667
Anastasiya Kruglaya <ariaignis.dragon@gmail.com>
2021-11-16 18:40:10 +0300

    representing all in html through alert

--- assets/js/myLib.js
+++ assets/js/myLib.js
@@ -1,4 +1,4 @@
-(function(global) {
+(function(window) {
 
     function myLibrary() {
         let _myLibraryObject = {};
@@ -61,15 +61,49 @@
         return _myLibraryObject;
     }
 
-    if (typeof(global.myLib) === 'undefined') {
-        global.myLib = myLibrary();
+    if (typeof(window.myLib) === 'undefined') {
+        window.myLib = myLibrary();
     }
 
-})(global); 
+})(window); 
 
 
 let array = [1, 3, '5', 7, 9 , 10];
-// console.log( myLib.isFunction(myLib.isArray) );
-// console.log( myLib.last(['1', '2', '3']) );
-// console.log( myLib.skip(array, 2).length);
-console.log( myLib.asChain(array).skip(1).take(2).array );
+alert(
+    "myLib.isArray([1, 2, 3]) => " + myLib.isArray([1, 2, 3]) +
+    "\nmyLib.isArray({}) => " + myLib.isArray({}));
+alert(
+    "myLib.isBoolean('') => " + myLib.isBoolean('') +
+    "\nmyLib.isBoolean(false) => " + myLib.isBoolean(false) );
+alert(
+    "myLib.isNumber(2000) => " + myLib.isNumber(2000) +
+    "\nmyLib.isNumber(\"45\") => " + myLib.isNumber("45") );
+alert(
+    "myLib.isDate('20.08.2021') => " + myLib.isDate('20.08.2021') +
+    "\nmyLib.isDate(new Date(2013, 13, 1)) => " + myLib.isDate(new Date(2013, 13, 1)) );  
+alert(
+    "myLib.isString('20.08.2021') => " + myLib.isString('20.08.2021') +
+    "\nmyLib.isString({}) => " + myLib.isString({}) );     
+alert(
+    "myLib.isUndefined(undefined) => " + myLib.isUndefined(undefined) +
+    "\nmyLib.isUndefined(null) => " + myLib.isUndefined(null) );  
+alert(
+    "myLib.isNull(0) => " + myLib.isNull(0) +
+    "\nmyLib.isNull(null)) => " + myLib.isNull(null) );     
+alert(
+    "myLib.isFunction('function') => " + myLib.isFunction('function') +
+    "\nmyLib.isFunction( () => prompt('How old are you?', '25') ) => " + myLib.isFunction( () => prompt('How old are you?', '25') ) );
+alert('Second part of the task: work with arrays');
+alert(
+    'let array = [1, 3, \'5\', 7, 9 , 10];\n' +
+    'myLib.first(array) => ' + myLib.first(array) +
+    '\nmyLib.last(array) => ' + myLib.last(array) +
+    '\nmyLib.skip(array, 3) => ' + myLib.skip(array, 3) +
+    '\nmyLib.take(array, 5) => ' + myLib.take(array, 5) 
+);
+array = [1, 3, '5', 7, 9 , 10];
+alert('Third part of the task: chaining');
+alert(
+    'let array = [1, 3, \'5\', 7, 9 , 10];\n' +
+    'myLib.asChain(array).skip(2).take(3).array => ' + myLib.asChain(array).skip(2).take(3).array
+);
