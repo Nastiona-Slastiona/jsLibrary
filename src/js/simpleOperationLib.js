de78ae30e2da938a1bdb7e8369b9857dce11fb5e
Anastasiya Kruglaya <ariaignis.dragon@gmail.com>
2021-12-13 14:08:26 +0300

    Corrected spaces

--- src/js/simpleOperationLib.js
+++ src/js/simpleOperationLib.js
@@ -4,6 +4,9 @@ const SHORT_LENGTH_MESSAGE = ' The length of the array is too short ';
 
 export function simpleOperationLibrary() {
     const simpleOpLib = {};
+    const isArray = obj => obj instanceof Array;
+    const isUndefined = obj => typeof obj == 'undefined';
+    const isNumber = obj => typeof obj == 'number';
 
     function isNotEmptyArray(arr) {
         if(arr.length === 0) {
@@ -14,14 +17,13 @@ export function simpleOperationLibrary() {
     }
 
     // First part of the task: type inditification 
-    simpleOpLib.isArray = obj =>
-        obj instanceof Array;
+    simpleOpLib.isArray = isArray;
 
     simpleOpLib.isBoolean = obj =>
         typeof obj == 'boolean';
 
-    simpleOpLib.isNumber = obj =>
-        typeof obj == 'number';
+    simpleOpLib.isNumber = isNumber;
+      
 
     simpleOpLib.isDate = obj =>
         obj instanceof Date;
@@ -29,8 +31,8 @@ export function simpleOperationLibrary() {
     simpleOpLib.isString = obj =>
         typeof obj == 'string';
     
-    simpleOpLib.isUndefined = obj =>
-        typeof obj == 'undefined';
+    simpleOpLib.isUndefined = isUndefined;
+       
 
     simpleOpLib.isNull = obj =>
         obj === null; 
@@ -40,34 +42,29 @@ export function simpleOperationLibrary() {
         
     // Work with arrays 
     simpleOpLib.first = arr => {
-        if(!simpleOpLib.isArray(arr)) {
+        if(!isArray(arr)) {
             return WRONG_PARAMETRS_MESSAGE;
         }
         
-        return isNotEmptyArray(arr) 
-            ? arr[0] 
-            : EMPTY_ARRAY_MESSAGE;
+        return isNotEmptyArray(arr) ? arr[0] : EMPTY_ARRAY_MESSAGE;
     }
 
     simpleOpLib.last = arr => {
-        if(!simpleOpLib.isArray(arr)) {
+        if(!isArray(arr)) {
             return WRONG_PARAMETRS_MESSAGE;
         }
 
-        return isNotEmptyArray(arr) 
-            ? arr[arr.length - 1] 
-            : EMPTY_ARRAY_MESSAGE;
+        return isNotEmptyArray(arr) ? arr[arr.length - 1] : EMPTY_ARRAY_MESSAGE;
     }
 
     simpleOpLib.skip = function(arr, number) {
-        if (simpleOpLib.isArray(arr)) {
+        if (isArray(arr)) {
             return arr.length > number 
-            ? arr.slice(number) 
-            : SHORT_LENGTH_MESSAGE;
+                        ? arr.slice(number) 
+                        : SHORT_LENGTH_MESSAGE;
         } 
         
-        if (simpleOpLib.isUndefined(number) 
-            || simpleOpLib.isNumber(arr)) {
+        if (isUndefined(number) || isNumber(arr)) {
             simpleOpLib.array =  simpleOpLib.skip(simpleOpLib.array, arr);
 
             return simpleOpLib;
@@ -77,16 +74,15 @@ export function simpleOperationLibrary() {
     };
 
     simpleOpLib.take = function(arr, number) {
-        if (simpleOpLib.isArray(arr)) {
+        if (isArray(arr)) {
             return  arr.length >= number 
-                    ? arr.slice(0, number) 
-                    : isNotEmptyArray(arr)
-                    ? SHORT_LENGTH_MESSAGE 
-                    : EMPTY_ARRAY_MESSAGE;
+                        ? arr.slice(0, number) 
+                        : isNotEmptyArray(arr)
+                            ? SHORT_LENGTH_MESSAGE 
+                            : EMPTY_ARRAY_MESSAGE;
         }
         
-        if (simpleOpLib.isUndefined(number) 
-            || simpleOpLib.isNumber(arr)) {
+        if (isUndefined(number) || isNumber(arr)) {
             simpleOpLib.array = simpleOpLib.take(simpleOpLib.array, arr);
 
             return simpleOpLib;
